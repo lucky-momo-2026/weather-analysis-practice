0efd271139f5fa8a09f45cb090756e7bf95f84ff
lucky-momo-2026 <grotta.di.azzura@gmail.com>
2026-04-02 23:33:46 +0900

    Pandasで降水量CSVを分析（全都市対応・ランキング追加）

--- analyze_rain.py
+++ analyze_rain.py
@@ -14,17 +14,38 @@ def main():
 
         df = pd.read_csv(csv_path)  #読み込むからread
         df = df.set_index(df.columns[0])  # df.columns は DataFrame列名　ここではcity の部分から呼び出す
+        month_total = df.sum()
 
-        tokyo = df.loc["東京"]
+        MONTH_MAP ={
+            "Jan": "1月", "Feb": "2月", "Mar": "3月",
+            "Apr": "4月", "May": "5月", "Jun": "6月",
+            "Jul": "7月", "Aug": "8月", "Sep": "9月",
+            "Oct": "10月", "Nov": "11月", "Dec": "12月"
+        } 
 
-        print('最大降水量：', tokyo.max())
-        print('最小降水量：', tokyo.min())
-        print('平均降水量：', tokyo.mean())
+        avg_rain = {}  #都市名・平均降水量をセットで入れる辞書
 
-        month = tokyo.idxmax()  # CSVが英語（月名）の場合は日本語に変換するために使う
-        jp_month = MONTHS[list(df.columns).index(month)]  # 今回rainfall.csv の中身が英語なので、MONTHS を使った変換が必要
- 
-        print('最大降水月：', jp_month)
+        for city, rain in df.iterrows():  # tokyo = df. loc[]は名前でその列のデータを抽出、for 〇, △ in df.iterrrows()は〇が行番号、△が行のデータ抽出
+
+            print(city, '最大降水量：', rain.max(), 'mm')  #cityがないとどこのデータがわからなくなるため必要
+            print(city, '最小降水量：', rain.min(), 'mm')
+
+            avg = round(rain.mean(), 1)  #辞書にその年の平均を出してavgに入れる
+            avg_rain[city] = avg  #辞書に保存する
+            print(city, '平均降水量：', avg, 'mm')  #上でavgという辞書を作ったのでavgに置き換え
+
+            month = rain.idxmax().capitalize()  # CSVが英語（月名）の場合は日本語に変換するために使う 全都市なのでrainに変更
+            jp_month = MONTH_MAP[month]
+            print(city, '最大降水月：', jp_month)
+            print()
+
+        top_city = max(avg_rain, key=avg_rain.get)  #値が一番大きい都市名を取り出す
+        print('平均降水量が最も多い都市：', top_city)
+        print('平均降水量：', avg_rain[top_city], 'mm')
+
+        top_month = month_total.idxmax()
+        jp_top_month = MONTH_MAP[top_month.capitalize()]
+        print('降水量が最も多い月：', jp_top_month)
 
     except Exception as e:
         print(f"csv読み込みエラー; {e}")
