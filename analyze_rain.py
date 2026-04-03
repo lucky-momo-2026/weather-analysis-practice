9cc7682e478a223acda7e3ce343aba30e1ef9298
lucky-momo-2026 <grotta.di.azzura@gmail.com>
2026-04-03 15:20:48 +0900

    都市別表示と集計処理を関数化

--- analyze_rain.py
+++ analyze_rain.py
@@ -3,22 +3,33 @@ import sys  #csvファイルを受取るので変わらない
 
 
 MONTHS = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
-
+# １都市分の降水量データを分析し、結果を表示して平均値を返す関数
 def print_city_report(city, rain, MONTH_MAP):
-    print(f' -----{city}----- ')
+    print(f' ------{city}------ ')
     print('最大降水量：', rain.max(), 'mm')
     print('最小降水量：', rain.min(), 'mm')
 
-    avg = round(rain.mean(), 1)
+    avg = round(rain.mean(), 1)  #平均降水量を少数点１桁で計算
     print('平均降水量：', avg, 'mm')
 
-    month = rain.idxmax().capitalize()
+    month = rain.idxmax().capitalize()  #降水量最大月（英語）を取得
     jp_month = MONTH_MAP[month]
     print('最大降水月：', jp_month)
     print()
 
     return avg
 
+# 全都市の平均降水量データから、最も多い都市を表示する関数
+def print_summary(avg_rain):
+    print(' ------集計結果------ ')
+
+    top_city = max(avg_rain, key=avg_rain.get)  # 平均降水量が一番大きい都市名を取得
+    print('平均降水量最多都市：', top_city)
+    print('平均降水量：', avg_rain[top_city], 'mm')
+
+    low_city = min(avg_rain,key=avg_rain.get)  #平均降水量が一番少ない都市名を取得
+    print('平均降水量最小都市：', low_city)
+    print('平均降水量：', avg_rain[low_city], 'mm')
 
 def main():
     if len(sys.argv) < 2:
@@ -43,15 +54,10 @@ def main():
         avg_rain = {}  #都市名・平均降水量をセットで入れる辞書
 
         for city, rain in df.iterrows():  # tokyo = df. loc[]は名前でその列のデータを抽出、for 〇, △ in df.iterrrows()は〇が行番号、△が行のデータ抽出
-  
             avg = print_city_report(city,rain, MONTH_MAP)
             avg_rain[city] = avg
 
-        print()
-        print(' -----集計結果-----')
-        top_city = max(avg_rain, key=avg_rain.get)
-        print('平均降水量が最も多い都市：', top_city)
-        print('平均降水量', avg_rain[top_city], 'mm')
+        print_summary(avg_rain)  #全年の集計結果を表示
 
     except Exception as e:
         print(f"csv読み込みエラー; {e}")
@@ -59,5 +65,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    # python analyze_rain.py rainfall.csv
